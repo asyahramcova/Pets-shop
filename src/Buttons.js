95a19b295aa0a6d6475f7f7e86ad2faf3d1f499e
asyahramcova <asyahramcova@gmail.com>
2023-02-27 20:11:24 +0300

    first commit

---
+++ src/Buttons.js
@@ -0,0 +1,16 @@
+
+function Buttons({chosenPet,petClothes}){
+
+   return(
+      <div className="cont" >
+         <button className="change"  onClick={() => chosenPet("cat")} >Cats</button>
+         <button className="change"  onClick={() => chosenPet("dog")} >Dogs</button>
+         <button className="change" onClick={() => petClothes("accessories")}  >Accessories</button>
+         <button className="change" onClick={() =>  petClothes("hoodie")} >Hoodies</button>
+         <button className="change" onClick={() => petClothes("pajamas")}>Pajamas</button>
+         <button className="change" onClick={() => petClothes("toys")}>Toys</button>
+      </div>
+   )
+}
+
+export default Buttons;
