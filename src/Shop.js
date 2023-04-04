14493d97c3454b86efe837cbbbe109a055694343
asyahramcova <118481943+asyahramcova@users.noreply.github.com>
2023-04-05 01:19:25 +0300

    Update Shop.js

--- src/Shop.js
+++ src/Shop.js
@@ -13,6 +13,9 @@ function Shop(){
       setClothes(newClothes);
    }
 
+   const allClothes = () =>{
+      setClothes(data)
+   }
    const chosenPet = (pet) =>{
       const onlyPet = data.filter(element => element.pet === pet);
       setClothes(onlyPet);
@@ -20,7 +23,7 @@ function Shop(){
 
    return(
       <div className="parent" >
-         <Buttons chosenPet = {chosenPet} petClothes= {chosenClothes}/>
+         <Buttons chosenPet = {chosenPet} petClothes= {chosenClothes} allClothes ={allClothes}/>
          <Clothes itemsForSale = {clothes} />
       </div>
    )
